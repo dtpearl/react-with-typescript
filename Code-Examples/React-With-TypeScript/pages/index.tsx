76e9f5f471812fbd9bf551e0830489723fdca823
Dima <d-dmytro@users.noreply.github.com>
2019-04-23 19:39:27 +0300

    ReactWithTypeScript: HOC Example with Redux

--- Code-Examples/React-With-TypeScript/pages/index.tsx
+++ Code-Examples/React-With-TypeScript/pages/index.tsx
@@ -1,1 +1,12 @@
-export default () => <div>Hello World!</div>;
+import { Provider } from 'react-redux';
+import { reducer } from '../reducer';
+import { createStore } from 'redux';
+import { ConnectedTasksList } from '../components/TasksList';
+
+const store = createStore(reducer);
+
+export default () => (
+  <Provider store={store}>
+    <ConnectedTasksList />
+  </Provider>
+);
