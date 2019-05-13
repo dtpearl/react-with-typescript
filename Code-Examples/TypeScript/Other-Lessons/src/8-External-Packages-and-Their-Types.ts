73e375c841376cedf471050b5abfa1a8508dc26b
Dmytro <d-dmytro@users.noreply.github.com>
2019-05-13 16:10:57 +0300

    Update 8-External-Packages-and-Their-Types.ts
    Added a descriptive comment to the 8-External-Packages-and-Their-Types.ts file.

--- Code-Examples/TypeScript/Other-Lessons/src/8-External-Packages-and-Their-Types.ts
+++ Code-Examples/TypeScript/Other-Lessons/src/8-External-Packages-and-Their-Types.ts
@@ -1,5 +1,6 @@
 import ApolloClient from 'apollo-client';
 import * as React from 'react';
 
-// Uncomment the react.d.ts file to fix this line.
+// !IMPORTANT: Open the "../react.d.ts" file and uncomment its content for this example to work.
+// The contents of "../react.d.ts" were commented to avoid code conflicts with other examples in this project.
 React.someFn();
